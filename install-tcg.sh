a3b730b3339ab6863811392030f61f2141f93fa9
Kamil Trzcinski <ayufan@ayufan.eu>
2024-03-04 23:44:52 +0100

    Add -nographic option

--- install-tcg.sh
+++ install-tcg.sh
@@ -12,7 +12,8 @@ wget -c https://download.mikrotik.com/routeros/$VERSION/mikrotik-$VERSION-arm64.
 
 exec qemu-system-aarch64 -m 1024 \
   -pflash /usr/share/AAVMF/AAVMF_CODE.fd -pflash efi-vars.qcow2 \
-  -vga none -serial chardev:term0 -chardev stdio,id=term0 \
+  -vga none -nographic -monitor none \
+  -serial chardev:term0 -chardev stdio,id=term0 \
   -cpu cortex-a72 -smp cpus=2,sockets=1,cores=2,threads=1 \
   -machine virt,virtualization=on -accel tcg,tb-size=128 \
   -device nvme,drive=hd0,serial=BDCF8C72-9BE7-4118-B274-EAD8B0982915,bootindex=1 \
--- run-tcg-user.sh
+++ run-tcg-user.sh
@@ -2,7 +2,8 @@
 
 exec qemu-system-aarch64 -m 1024 \
   -pflash /usr/share/AAVMF/AAVMF_CODE.fd -pflash efi-vars.qcow2 \
-  -vga none -serial chardev:term0 -chardev stdio,id=term0 \
+  -vga none -nographic -monitor none \
+  -serial chardev:term0 -chardev stdio,id=term0 \
   -cpu cortex-a72 -smp cpus=2,sockets=1,cores=2,threads=1 \
   -machine virt,virtualization=on -accel tcg,tb-size=128 \
   -device nvme,drive=hd0,serial=BDCF8C72-9BE7-4118-B274-EAD8B0982915,bootindex=0 \
