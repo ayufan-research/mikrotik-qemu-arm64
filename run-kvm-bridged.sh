59778ec4aab246dc41589366a5838e40b02523d9
Kamil Trzciński <ayufan@ayufan.eu>
2025-01-05 20:28:47 +0100

    Update run-kvm-bridged.sh

--- run-kvm-bridged.sh
+++ run-kvm-bridged.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+BR=br0
+
+if ip link show br-lan &>/dev/null; then
+  BR=br-lan
+fi
+
 exec qemu-system-aarch64 -m 1024 \
   -pflash firmware/AAVMF_CODE.fd -pflash efi-vars.qcow2 \
   -vga none -nographic -monitor none \
@@ -9,5 +15,5 @@ exec qemu-system-aarch64 -m 1024 \
   -device nvme,drive=hd0,serial=BDCF8C72-9BE7-4118-B274-EAD8B0982915,bootindex=0 \
   -drive if=none,file=root.qcow2,id=hd0,media=disk,discard=unmap,detect-zeroes=unmap \
   -device virtio-net-pci,netdev=netdev0 \
-  -netdev tap,id=netdev0 \
+  -netdev bridge,br=$BR,id=netdev0 \
   "$@"
