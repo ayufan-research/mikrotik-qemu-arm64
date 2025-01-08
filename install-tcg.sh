2d70ee9cc73d00f86305c5684a3c8fcc29d5a322
Kamil Trzcinski <ayufan@ayufan.eu>
2025-01-08 21:40:03 +0100

    Configure MEM, CPUS, DISK

--- install-tcg.sh
+++ install-tcg.sh
@@ -5,14 +5,13 @@ source "$SCRIPT_DIR/helpers"
 
 wget -c https://download.mikrotik.com/routeros/$VERSION/mikrotik-$VERSION-arm64.iso
 
-[[ -e efi-vars.qcow2 ]] || qemu-img create efi-vars.qcow2 64M
 [[ -e root.qcow2 ]] || qemu-img create root.qcow2 1G
 
-exec qemu-system-aarch64 -m 1024 \
-  -pflash AAVMF_CODE.fd -pflash efi-vars.qcow2 \
+exec qemu-system-aarch64 -m "${MEM:-256}" \
+  -pflash AAVMF_CODE.fd \
   -vga none -nographic -monitor none \
   -serial chardev:term0 -chardev stdio,id=term0 \
-  -cpu cortex-a72 -smp cpus=2,sockets=1,cores=2,threads=1 \
+  -cpu cortex-a72 -smp cpus=1,sockets=1,cores=1,threads=1 \
   -machine virt,virtualization=on -accel tcg,tb-size=128 \
   -device nvme,drive=hd0,serial=BDCF8C72-9BE7-4118-B274-EAD8B0982915,bootindex=1 \
   -drive if=none,file=root.qcow2,id=hd0,media=disk,discard=unmap,detect-zeroes=unmap \
--- run-kvm-bridged.sh
+++ run-kvm-bridged.sh
@@ -9,11 +9,11 @@ if ip link show br-lan &>/dev/null; then
   BR=br-lan
 fi
 
-exec qemu-system-aarch64 -m 1024 \
-  -pflash AAVMF_CODE.fd -pflash efi-vars.qcow2 \
+exec qemu-system-aarch64 -m "${MEM:-256}" \
+  -pflash AAVMF_CODE.fd \
   -vga none -nographic -monitor none \
   -serial chardev:term0 -chardev stdio,id=term0 \
-  -cpu host -smp cpus=2,sockets=1,cores=2,threads=1 \
+  -cpu host -smp "cpus=${CPUS:-2},sockets=1,cores=${CPUS:-2},threads=1" \
   -machine virt,accel=kvm \
   -device nvme,drive=hd0,serial=BDCF8C72-9BE7-4118-B274-EAD8B0982915,bootindex=0 \
   -drive if=none,file=root.qcow2,id=hd0,media=disk,discard=unmap,detect-zeroes=unmap \
